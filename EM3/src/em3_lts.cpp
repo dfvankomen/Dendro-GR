768c0b8cd101710c40a33120f56d98bb9ed9287e
David Van Komen <david.vankomen@gmail.com>
2025-05-09 11:06:05 -0600

    format[auto]: autoformatting before sync

--- EM3/src/em3_lts.cpp
+++ EM3/src/em3_lts.cpp
@@ -224,10 +224,10 @@ int main(int argc, char** argv) {
 
             if ((step % em3::EM3_CHECKPT_FREQ) == 0) appCtx->write_checkpt();
 
-                // appCtx_ets->dump_pt(std::cout);
-                // appCtx_enuts->dump_pt(std::cout);
-                // ets->dump_pt(std::cout);
-                // enuts->dump_pt(std::cout);
+            // appCtx_ets->dump_pt(std::cout);
+            // appCtx_enuts->dump_pt(std::cout);
+            // ets->dump_pt(std::cout);
+            // enuts->dump_pt(std::cout);
 
 #ifdef __PROFILE_ETS__
             char fName[200];
