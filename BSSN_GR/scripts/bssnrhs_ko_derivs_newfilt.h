3ebdcd85cd8490f2b6c0cbb47de484d16d1c2fb9
David Van Komen <david.vankomen@gmail.com>
2025-05-14 11:40:34 -0600

    feat: fix new KO filtering and remove bssnSolverCtx from cmake

--- BSSN_GR/scripts/bssnrhs_ko_derivs_newfilt.h
+++ BSSN_GR/scripts/bssnrhs_ko_derivs_newfilt.h
@@ -1,59 +1,59 @@
 if (bssn::BSSN_DERIVS->get_filter_family() ==
     dendroderivs::FilterFamily::FF_KO) {
     // for KO diss, input/output should be the same!
-    bssn::BSSN_DERIVS->filter(a_rhs, a_rhs, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+    bssn::BSSN_DERIVS->filter(alpha, a_rhs, grad_0_gt0, grad_1_gt0, grad_2_gt0,
                               hx, hy, hz, sigma, sz, bflag);
 
-    bssn::BSSN_DERIVS->filter(chi_rhs, chi_rhs, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-
-    bssn::BSSN_DERIVS->filter(K_rhs, K_rhs, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+    bssn::BSSN_DERIVS->filter(chi, chi_rhs, grad_0_gt0, grad_1_gt0, grad_2_gt0,
                               hx, hy, hz, sigma, sz, bflag);
 
-    bssn::BSSN_DERIVS->filter(gt_rhs00, gt_rhs00, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(gt_rhs01, gt_rhs01, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(gt_rhs02, gt_rhs02, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(gt_rhs11, gt_rhs11, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(gt_rhs12, gt_rhs12, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(gt_rhs22, gt_rhs22, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(K, K_rhs, grad_0_gt0, grad_1_gt0, grad_2_gt0, hx,
+                              hy, hz, sigma, sz, bflag);
 
-    bssn::BSSN_DERIVS->filter(b_rhs0, b_rhs0, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(b_rhs1, b_rhs1, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(b_rhs2, b_rhs2, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(gt0, gt_rhs00, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(gt1, gt_rhs01, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(gt2, gt_rhs02, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(gt3, gt_rhs11, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(gt4, gt_rhs12, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(gt5, gt_rhs22, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
 
-    bssn::BSSN_DERIVS->filter(At_rhs00, At_rhs00, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(At_rhs01, At_rhs01, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(At_rhs02, At_rhs02, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(At_rhs11, At_rhs11, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(At_rhs12, At_rhs12, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(At_rhs22, At_rhs22, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(beta0, b_rhs0, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(beta1, b_rhs1, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(beta2, b_rhs2, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
 
-    bssn::BSSN_DERIVS->filter(Gt_rhs0, Gt_rhs0, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(Gt_rhs1, Gt_rhs1, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(Gt_rhs2, Gt_rhs2, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(At0, At_rhs00, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(At1, At_rhs01, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(At2, At_rhs02, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(At3, At_rhs11, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(At4, At_rhs12, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(At5, At_rhs22, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
 
-    bssn::BSSN_DERIVS->filter(B_rhs0, B_rhs0, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(B_rhs1, B_rhs1, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
-    bssn::BSSN_DERIVS->filter(B_rhs2, B_rhs2, grad_0_gt0, grad_1_gt0,
-                              grad_2_gt0, hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(Gt0, Gt_rhs0, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(Gt1, Gt_rhs1, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(Gt2, Gt_rhs2, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+
+    bssn::BSSN_DERIVS->filter(B0, B_rhs0, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(B1, B_rhs1, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
+    bssn::BSSN_DERIVS->filter(B2, B_rhs2, grad_0_gt0, grad_1_gt0, grad_2_gt0,
+                              hx, hy, hz, sigma, sz, bflag);
 }
